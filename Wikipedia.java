06fb8cee0c2757950a600af4df0c82221a17df61
JeremiahPinto <jeremiahpinto279@outlook.com>
2017-05-03 21:14:26 +0800

    Skeleton project

---
+++ Wikipedia.java
@@ -0,0 +1,46 @@
+
+/**
+ * @author Jeremiah Pinto(21545883) and Neosh Sheikh(21959462)
+ *
+ */
+public class Wikipedia implements CITS2200Project
+{
+
+	/**
+	 * 
+	 */
+	public Wikipedia() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void addEdge(String urlFrom, String urlTo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getShortestPath(String urlFrom, String urlTo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String[] getCenters() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[][] getStronglyConnectedComponents() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[] getHamiltonianPath() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

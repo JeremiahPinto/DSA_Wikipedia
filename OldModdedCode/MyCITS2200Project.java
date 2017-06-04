8ede3086f81960b8478ec2e92c0cefe44c59d83b
JeremiahPinto <jeremiahpinto279@outlook.com>
2017-06-04 14:45:12 +0800

    Update MyCITS2200Project.java

--- OldModdedCode/MyCITS2200Project.java
+++ OldModdedCode/MyCITS2200Project.java
@@ -11,8 +11,8 @@ import java.util.LinkedList;
 //Class that contains the contents of the graph
 class Graph
 {
-	public ArrayList<LinkedList<Integer>> p; //Adjacency list of the edges in the graph
-	public LinkedList<String> s;			  //List of all the vertices
+	private ArrayList<LinkedList<Integer>> p; //Adjacency list of the edges in the graph
+	private LinkedList<String> s;			  //List of all the vertices
 	public static final int INFINITY = Integer.MAX_VALUE/2;
 
 	Graph()
@@ -23,7 +23,7 @@ class Graph
 
 	//A method that adds and edge between the "from" and "to" vertices,
 	//if either doesn't exist, it creates them
-	void addEdge(String from, String to)
+	void addEdge( String from, String to )
 	{
 		int locF = -1; int locT = -1; //Variables to get the indices of the "from" and "to" vertices if present
 
@@ -32,11 +32,11 @@ class Graph
 			if( s.get(i).equals(to) )
 				locT = i;
 
-			if( s.get(i).equals(from))
+			if( s.get(i).equals(from) )
 				locF = i;
 
 			//Break the loop if both, "from" and "to" are found
-			if(locT != -1 && locF != -1) 
+			if( locT != -1 && locF != -1 ) 
 				break;
 		}
 
@@ -69,40 +69,39 @@ class Graph
 
 	//A method to return the index when the vertex is passed as a parameter,
 	//if not present, returns -1
-	int indexOf(String str)
+	int indexOf( String str )
 	{
 		for(int i = 0; i< s.size(); i++)
-			if(s.get(i).equals(str)) 
+			if( s.get(i).equals(str) ) 
 				return i;
+		
 		return -1;
 	}
 
 	//A method that returns 1 if the parameter "b" is present in the row of the adjacency list of the parent "a",
 	//else it returns INFINITY
-	int dist(int a, int b)
+	int dist( int a, int b )
 	{
-		if(p.get(a).contains(b)) 
+		if( p.get(a).contains(b) ) 
 			return 1;
+		
 		return INFINITY;
 	}
 
-	String getVertex(int i) 
+	String getVertex( int i ) 
 	{	return s.get(i);	}
 
-	//	ArrayList<LinkedList<Integer>> getEdge() //
-	//	{	return p;	}
-
-	LinkedList<Integer> getRow(int i) 
+	LinkedList<Integer> getRow( int i ) 
 	{	return p.get(i);	}
 
 	int size()
 	{	return s.size();	}
 
-	static Graph transpose(Graph G)
+	static Graph transpose( Graph G )
 	{
 		Graph RG = new Graph();
 
-		for( int i = 0; i < G.size();i++ )
+		for( int i = 0; i < G.size(); i++ )
 		{
 			LinkedList<Integer> temp = new LinkedList<Integer>();
 
@@ -114,7 +113,7 @@ class Graph
 		for( LinkedList<Integer> x : G.p )
 			for( int child : x )
 				if( child != x.get(0) ) 
-					RG.addEdge(G.getVertex(child), G.getVertex(x.get(0)) );
+					RG.addEdge( G.getVertex(child), G.getVertex(x.get(0)) );
 
 		return RG;
 	}
@@ -123,36 +122,32 @@ public class MyCITS2200Project implements CITS2200Project
 {
 
 	public Graph page;
-	public static final int INFINITY = Integer.MAX_VALUE/2;
+	
 	public MyCITS2200Project()
 	{	page = new Graph();	}
 
 	//A method that adds an edge from the "from" vertex to the "to" vertex
 	@Override
-	public void addEdge(String from, String to) 
+	public void addEdge( String from, String to ) 
 	{	page.addEdge(from, to);	}
-	
-	public int getNum(String s)
-	{
-		int i = -1;
-		if(page.s.contains(s))page.s.indexOf(s);
-		return i;
-	}
 
 	//A method that gets the shortest path from the "from" vertex to the "to" vertex
 	//using a breadth first search
 	@Override
-	public int getShortestPath(String urlFrom, String urlTo) 
+	public int getShortestPath( String urlFrom, String urlTo ) 
 	{
 		boolean contFrom = false, contTo = false; 
 		int from = 0;
-		if(urlFrom.equals(urlTo)) return 0;
+		
+		if(urlFrom.equals(urlTo)) 
+			return 0;
+			
 		for( int i = 0; i<page.size(); i++ )
 		{
 			if( page.getVertex(i).equals(urlTo) )	
 				contTo = true;	
 
-			if( page.getVertex(i).equals(urlFrom))
+			if( page.getVertex(i).equals(urlFrom) )
 			{
 				contFrom = true;
 				from = i;
@@ -169,6 +164,7 @@ public class MyCITS2200Project implements CITS2200Project
 		int visited[] = new int[page.size()];
 		LinkedList<Integer> queue = new LinkedList<Integer>();
 		int disA_B = -1;
+		
 		for(int x = 0; x < dist.length; x++)
 			dist[x] = visited[x] = -1;
 		
@@ -186,20 +182,21 @@ public class MyCITS2200Project implements CITS2200Project
 				if( index != x && visited[index] == -1 )
 				{
 					visited[index] = x;
-
 					queue.add(index);
 					dist[index] = dist[x]+1;
 				}
+				
 				//Finds the first instance of urlTo and if found, assigns distance to distA_B
 				//Breaks out of the loop if it's found
 				if( index != x && page.getVertex(index).equals(urlTo) )
 				{
-					queue.clear();		//used to exit out of the while loop
+					queue.clear();
 					disA_B = dist[index];
 					break;
 				}
 			}
 		}
+		
 		return disA_B;
 	}
 
@@ -208,85 +205,85 @@ public class MyCITS2200Project implements CITS2200Project
 	 * starting from the "from" vertex until the end of the graph
 	 * 
 	 * @param from, the vertex from which the BFS must start
+	 * @return the distance from the vertex from to the end of the graph
 	 */
-	private int findE(int from)
+	private int findE( int from )
 	{
 		
 		int dist[] = new int[page.size()];
 		int visited[] = new int[page.size()];
 		
-		LinkedList<Integer> q = new LinkedList<Integer>(); 	//q is a queue
+		LinkedList<Integer> queue = new LinkedList<Integer>();
 		int maxD = 0;
+		
 		for(int x = 0; x < dist.length; x++)
 			dist[x] = visited[x] = -1;
 
 		int count = 0;
-		visited[from]= -2; 
-		dist[from]=0; 
+		visited[from] = -2; 
+		dist[from] = 0; 
 		
-		q.add(from);
+		queue.add(from);
 		
-		while(!q.isEmpty())
+		while( !queue.isEmpty() )
 		{
-			int x = q.remove();
+			int x = queue.remove();
 			
 			// checks the size of the linked-list at x and it's children 
-			for(int index : page.getRow(x))
+			for( int index : page.getRow(x) )
 			{
-				if(index == x) continue;
-				if(visited[index] == -1)
+				if( index == x ) 
+					continue;
+				if( visited[index] == -1 )
 				{
 					count++;
-					visited[index]=x;
-					q.add(index);
-					dist[index]=dist[x]+1;
-					if(dist[index]>maxD)
-						maxD=dist[index];
+					visited[index] = x;
+					queue.add(index);
+					dist[index] = dist[x]+1;
+					
+					if( dist[index] > maxD )
+						maxD = dist[index];
 				}
 			}
 		}
 
-//		for(int i = 0; i < visited.length; i++)
-//			if(dist[i]==-1) return Integer.MAX_VALUE;
-		if(count< page.size() -1) return INFINITY;
+		if( count < page.size()-1 ) 
+			return Graph.INFINITY;
+			
 		return maxD;
 	}
 
-	/**
-	 * Finds all the centers of the page graph. The order of pages
-	 * in the output does not matter. Any order is correct as long as
-	 * all the centers are in the array, and no pages that aren't centers
-	 * are in the array.
-	 * 
-	 * @return an array containing all the URLs that correspond to pages that are centers.
-	 */
-	
+	//Finds the centers of the graph using the helper method findE() for each vertex
 	@Override
 	public String[] getCenters() 
 	{
 		
 		String[] result;
 		int[] ecc = new int[page.size()];
-		int minMax = INFINITY;
+		int minMax = Graph.INFINITY;
 		
 		ArrayList<Integer> storage = new ArrayList<Integer>();
-		for( int i = 0 ; i<page.size();i++)
+		for( int i = 0 ; i<page.size(); i++ )
 		{
-			ecc[i]=findE(i);
-			if(ecc[i]<=minMax && ecc[i] != INFINITY)
+			ecc[i] =findE(i);
+			
+			if (ecc[i] <= minMax && ecc[i] != Graph.INFINITY )
 			{
-				if(ecc[i]<minMax)
+				if(ecc[i] < minMax)
 				{
 					storage.clear();
 					minMax = ecc[i];
 				}
+				
 				storage.add(i);
 			}
 		}
 		
 		result = new String[storage.size()];
-		for(int j = 0; j<storage.size();j++)
+		
+		for( int j = 0; j<storage.size(); j++ )
 			result[j] = page.getVertex(storage.get(j));
+			
 		return result;
 	}
 
@@ -295,41 +292,46 @@ public class MyCITS2200Project implements CITS2200Project
 	public String[][] getStronglyConnectedComponents() 
 	{
 		int size = page.size();
-		boolean[] used = new boolean[size];
-		
+		boolean[] used = new boolean[size];		
 		ArrayList<Integer> order = new ArrayList<Integer>();
-		for(int i = 0; i < size; i++)
-			if(!used[page.getRow(i).get(0)]){
+		
+		for( int i = 0; i < size; i++ )
+			if( !used[page.getRow(i).get(0)] )
 				dfs(page, used, order, page.getRow(i).get(0));
-			}
 		
 		Graph Tpage = Graph.transpose(page);
 		ArrayList<ArrayList<Integer>> comp = new ArrayList<ArrayList<Integer>>();
-
-
+		
 		Arrays.fill(used, false);
-	    Collections.reverse(order);
-	    int sizeA=0;
+		Collections.reverse(order);
+		int sizeA = 0;
 	    
-	    for (int i = 0; i < size; i++)
-	        if (!used[order.get(i)]) {
-	          ArrayList<Integer> component = new ArrayList<>();
-	          dfs(Tpage, used, component, order.get(i));
-	          if(component.size()>sizeA) sizeA = component.size();
-	          comp.add(component);
-	        }
-	    String[][] re = new String[comp.size()][sizeA];
-	    int countB;
-		int countA = countB = 0;
-	    for(ArrayList<Integer> ape : comp)
-	    {
-	    	countB = 0;
-	    	for(int val : ape){
-	    		re[countA][countB] = page.getVertex(comp.get(countA).get(countB));
-	    	countB++;	
+	    	for ( int i = 0; i < size; i++ )
+	        	if (!used[order.get(i)]) 
+			{
+	         		ArrayList<Integer> component = new ArrayList<>();
+	          		dfs(Tpage, used, component, order.get(i));
+				
+	         		if( component.size() > sizeA) 
+					sizeA = component.size();
+					
+	          		comp.add(component);
+	        	}
+			
+	    	String[][] re = new String[comp.size()][sizeA];
+		int countA = 0, countB = 0;
+		
+	    	for( ArrayList<Integer> ape : comp )
+	    	{
+	    		countB = 0;
+	    		for( int val : ape )
+			{
+	    			re[countA][countB] = page.getVertex(comp.get(countA).get(countB));
+	    			countB++;	
+	    		}
+	    		countA++;
 	    	}
-	    	countA++;
-	    }
+		
 		return re;
 	}
 
@@ -353,7 +355,6 @@ public class MyCITS2200Project implements CITS2200Project
 			ord.add(v);
 	}
 
-	boolean check;
 	// Finds the Hamiltonian path using the Held-Karp algorithm
 	@Override
 	public String[] getHamiltonianPath() 
@@ -361,32 +362,7 @@ public class MyCITS2200Project implements CITS2200Project
 		int n = page.size();		
 		int[][]  dp = new int [1 << n][n];
 
-		//		//debug****
-		//int[][] adj = new int [n][n];
-		//		for(LinkedList<Integer> i : page.getEdge())
-		//			for(int j = 1; j < i.size(); j++)
-		//				adj[i.get(0)][i.get(j)] = 1;
-		//
-		//		
-		//		System.out.print(" \t");
-		//		for(int i = 0; i < n; i++)
-		//			if(page.getVertex(i).toCharArray().length == 1)
-		//				System.out.print(page.getVertex(i) + "  ");
-		//			else
-		//				System.out.print(page.getVertex(i) + " ");
-		//		System.out.println();
-		//
-		//		for (int i = 0; i < adj.length; i++) {
-		//			System.out.print(page.getVertex(i) + "\t");
-		//			for (int j = 0; j < adj[i].length; j++) 
-		//				System.out.print(adj[i][j] + "  ");
-		//
-		//			System.out.println();
-		//
-		//		}
-		//		System.out.println();
-		//		//********
-
+		//********** Code referenced from Hackerearth *******************
 		for ( int[] d : dp )
 			Arrays.fill(d, Graph.INFINITY);
 
@@ -399,9 +375,11 @@ public class MyCITS2200Project implements CITS2200Project
 					for ( int j = 0; j < n; j++ ) 
 						if ( (mask & 1 << j) != 0 ) 
 							dp[mask][i] = Math.min(dp[mask][i], dp[mask^(1 << i)][j] + page.dist(j,i));	
-
-		// reconstruct path
-
+		
+		//***************************************************************
+		
+		// Backtracking to reconstruct path
+		
 		int[] order = new int[n];
 		int last = -1, cur = (1 << n) - 1, bj;
 
@@ -430,38 +408,4 @@ public class MyCITS2200Project implements CITS2200Project
 
 		return ham;
 	}
-	int max =0;
-	private void ham(Graph G, LinkedList<Integer> path, int vertex)
-	{
-		path.add(vertex);
-		if(path.size() == G.size()){
-			check = false;
-		}
-
-		for(int x = 1; x < G.getRow(vertex).size(); x++) //Children of vertex
-		{
-
-			if(!path.contains(G.getRow(vertex).get(x)))
-			{
-				ham(G, path, G.getRow(vertex).get(x));
-				if(!check) break;
-			}
-		}
-		if(path.size()>=max){
-		System.out.println(path + " " + path.size());
-		max = path.size();
-		}
-		if(check) path.removeLast();
-	}
-	
-	public static void printGraph(Graph G)
-	{
-		for(LinkedList<Integer> s: G.p)
-		{
-		for(Integer p: s)
-			System.out.print(G.s.get(p) +"\t");
-		System.out.println();
-		}
-	}
-
 }
